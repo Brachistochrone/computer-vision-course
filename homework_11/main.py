52165762a77760edb1cf422f3c8b575932eb654c
alikhomanov <Artem.Likhomanov@dataart.com>
2022-11-11 00:09:27 +0200

    Homework #11 code finish

--- homework_11/main.py
+++ homework_11/main.py
@@ -1,31 +1,65 @@
-import cv2
-import numpy as np
 import matplotlib
+import numpy as np
 import pandas as pd
+from matplotlib import pyplot as plt
 from numpy.random import seed
-from tensorflow.python.framework.random_seed import set_seed
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+from tensorflow.python.framework.random_seed import set_seed
 from tensorflow.python.keras import Model, metrics
-from tensorflow.python.keras.layers import Dense, Dropout, Input
-from tensorflow.python.keras.models import Sequential
-from matplotlib import pyplot as plt
+from tensorflow.python.keras.layers import Dense, Input
 
 matplotlib.use('TkAgg')
 plt.rcParams['figure.figsize'] = [10, 5]
 
 
-def plot_history(history, coef):
+def prepare_and_normalize(dataset, keys):
+    # get attributes as inputs
+    data = dataset[keys]
+    # fill the missing values with the mean value of the dataset
+    data = data.fillna(data.mean())
+    # normalize data
+    x = data[keys[1:]]
+    scale = StandardScaler()
+    x = scale.fit_transform(x)
+    y = data[keys[0]].values
+    y = y / 100000
+    # split data into 75% for train and 25% for test
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.25)
+    return x_train, x_test, y_train, y_test
+
+
+def build_network(input_num, layer_num = 1, neuron_num = 1, title = ''):
+    # configure input layer
+    ins = Input(shape = input_num)
+    # add layers
+    layers = None
+    for ln in range(layer_num):
+        if ln == 0:
+            layers = Dense(neuron_num, activation = 'relu')(ins)
+        else:
+            layers = Dense(neuron_num, activation = 'relu')(layers)
+    # configure output layer
+    layers = Dense(1, activation = 'linear')(layers)
+    # create network
+    network = Model(ins, layers)
+    network._name = title
+    # compile network
+    network.compile(optimizer = 'adam', loss = 'mean_squared_error', metrics = [metrics.mae])
+    return network
+
+
+def plot_history(history, coef, name = ''):
     h = history.history
     epochs = range(len(h['loss']))
 
     plt.subplot(121), plt.plot(epochs, h['loss'], '.-', epochs, h['val_loss'], '.-')
     plt.grid(True), plt.xlabel('epochs'), plt.ylabel('loss')
-    plt.legend(['Train', 'Validation'])
+    plt.legend(['Train', 'Validation']), plt.title(name + ' MSE')
     plt.subplot(122), plt.plot(epochs, np.array(h['mean_absolute_error']) * coef, '.-',
                                epochs, np.array(h['val_mean_absolute_error']) * coef, '.-')
     plt.grid(True), plt.xlabel('epochs'), plt.ylabel('MAE')
-    plt.legend(['Train', 'Validation'])
+    plt.legend(['Train', 'Validation']), plt.title(name + ' MAE')
 
     print('Train MAE     ', h['mean_absolute_error'][-1] * coef)
     print('Validation MAE', h['val_mean_absolute_error'][-1] * coef)
@@ -45,16 +79,47 @@ dataset = pd.read_csv('resources/train.csv')
 
 # get attributes as inputs
 features = ['SalePrice', 'OverallQual', 'GrLivArea', 'GarageCars', 'FullBath', 'YearBuilt']
-data = dataset[features]
-# fill the missing values with the mean value of the dataset
-data = data.fillna(data.mean())
-# normalize data
-inputs = data[features[1:]]
-scale = StandardScaler()
-inputs = scale.fit_transform(inputs)
-outputs = data[features[0]].values
-outputs = outputs / 100000
-# split data into 75% for train and 25% for test
-inputs_train, inputs_test, outputs_train, outputs_test = train_test_split(inputs, outputs, test_size = 0.25)
+inputs_train, inputs_test, outputs_train, outputs_test = prepare_and_normalize(dataset, features)
 
 # Building the Network
+
+# create network (one layers 5 neurons)
+network = build_network(len(features) - 1, 1, 5, 'Initial_network')
+network.compile(optimizer = 'adam', loss = 'mean_squared_error', metrics = [metrics.mae])
+print(network.summary())
+
+# Training the Network
+
+# train network
+history = network.fit(inputs_train, outputs_train, validation_data = (inputs_test, outputs_test), epochs = 150,
+                      batch_size = 32, verbose = 0)
+plot_history(history, 1e5, 'Initial network')
+
+# Improving the Network
+
+# add more input parameters (+'LotArea')
+features = ['SalePrice', 'OverallQual', 'GrLivArea', 'GarageCars', 'FullBath', 'YearBuilt', 'LotArea']
+inputs_train, inputs_test, outputs_train, outputs_test = prepare_and_normalize(dataset, features)
+network = build_network(len(features) - 1, 1, 5, 'Network_with_more_inputs')
+print(network.summary())
+history = network.fit(inputs_train, outputs_train, validation_data = (inputs_test, outputs_test), epochs = 150,
+                      batch_size = 32, verbose = 0)
+plot_history(history, 1e5, 'Network with more inputs')
+
+# increase the number of neurons in the layer (1x10)
+features = ['SalePrice', 'OverallQual', 'GrLivArea', 'GarageCars', 'FullBath', 'YearBuilt']
+inputs_train, inputs_test, outputs_train, outputs_test = prepare_and_normalize(dataset, features)
+network = build_network(len(features) - 1, 1, 10, 'Network_with_more_neurons')
+print(network.summary())
+history = network.fit(inputs_train, outputs_train, validation_data = (inputs_test, outputs_test), epochs = 150,
+                      batch_size = 32, verbose = 0)
+plot_history(history, 1e5, 'Network with more neurons')
+
+# increase the number of layers (2x5)
+features = ['SalePrice', 'OverallQual', 'GrLivArea', 'GarageCars', 'FullBath', 'YearBuilt']
+inputs_train, inputs_test, outputs_train, outputs_test = prepare_and_normalize(dataset, features)
+network = build_network(len(features) - 1, 2, 5, 'Network_with_more_layers')
+print(network.summary())
+history = network.fit(inputs_train, outputs_train, validation_data = (inputs_test, outputs_test), epochs = 150,
+                      batch_size = 32, verbose = 0)
+plot_history(history, 1e5, 'Network with more layers')
